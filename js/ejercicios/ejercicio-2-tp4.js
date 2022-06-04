baf373f5b8bd2030f36162321367b4dd01f87c47
Daniela Cardozo <mdanielacardozo@gmail.com>
2022-06-03 22:08:07 -0300

    Ejercicio 3 TP4 check

--- js/ejercicios/ejercicio-2-tp4.js
+++ js/ejercicios/ejercicio-2-tp4.js
@@ -1,1 +1,29 @@
+/* 2-Escribe un programa que cree un objeto "cuenta" con las siguientes propiedades:
+Una propiedad titular con el valor "Alex".
+Una propiedad saldo, teniendo como valor inicial 0.
+Un método ingresar() que permita añadir dinero a la cuenta, pasando la cantidad como parámetro
+Un método extraer() que permita retirar la cantidad pasada como parámetro.
+Un método informar() que retorne la información del estado de la cuenta. 
 
+Utiliza este objeto para mostrar la descripción, ingresar y extraer dinero y volver a mostrar la 
+descripción del estado de la cuenta.
+Rectángulos*/
+
+class Cuenta{
+    constructor(propiedadTitular, propiedadSaldo, propiedadEstado){
+        this.propiedadTitular = propiedadTitular;
+        this.propiedadSaldo = propiedadSaldo; 
+        this.propiedadEstado = propiedadEstado;
+    }
+    ingresar(){
+
+    }
+    extraer(){
+
+    }
+    informar(){
+
+    }
+}
+
+let cuentaAlex = new Cuenta('Alex', '0',)
--- js/ejercicios/ejercicio-3-tp4.js
+++ js/ejercicios/ejercicio-3-tp4.js
@@ -1,2 +1,30 @@
+/*3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, 
+mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área*/
 
+
+
+class Rectangulo {
+    constructor ( altura = 0, ancho = 0 ) {
+      this._altura = altura;
+      this._width = ancho;
+    }
+  
+    set cambiarAltura( valor ) {
+      this._altura = valor;
+    }
+  
+    set cambiarAncho( valor ) {
+      this._ancho = valor;
+    }
+  
+    get perimetro () {
+        return (this._altura * 2) + (this._ancho * 2);
+    }
+
+    get area() {
+      return this._altura * this._ancho;
+    }
+  }
+
+  let rectanguloUno = new Rectangulo(2, 4);
   
